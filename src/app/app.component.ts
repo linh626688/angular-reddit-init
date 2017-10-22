5e12ec8ed078942c117914c23d14d48a4bbe46c1
linh626688@gmail.com <cavoixanh>
2017-10-22 22:14:36 +0700

    init app angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Article} from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value,link.value,0));
+    title.value='';
+    link.value='';
+    return false;
+  }
+  articles :Article[];
+  constructor(){
+    this.articles = [new Article('Angular 2', 'http://angular.io', 10),
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Angular Homepage', 'http://angular.io', 1),];
+  }
+
+  sortedArticles(): Article[]{
+    return this.articles.sort((a: Article, b: Article)=> b.votes - a.votes);
+  }
 }
