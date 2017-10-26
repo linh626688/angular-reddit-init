6d7d632998712bb1c5aa21c9c8411b287f9132fe
linhdt-java <linh626688@gmail.com>
2017-10-26 17:15:39 +0700

    doing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,29 @@ import {Article} from "./article/article.model";
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.articles.push(new Article(title.value,link.value,0));
-    title.value='';
-    link.value='';
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
-  articles :Article[];
-  constructor(){
+
+  articles: Article[];
+  choice: number;
+
+  constructor() {
+    this.choice = 0;
     this.articles = [new Article('Angular 2', 'http://angular.io', 10),
       new Article('Angular 2', 'http://angular.io', 3),
       new Article('Angular 2', 'http://angular.io', 3),
       new Article('Angular Homepage', 'http://angular.io', 1),];
   }
 
-  sortedArticles(): Article[]{
-    return this.articles.sort((a: Article, b: Article)=> b.votes - a.votes);
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+  nextChoice(): boolean {
+    this.choice += 1;
+    return false;
   }
 }
